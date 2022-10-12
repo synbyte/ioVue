1cac859873683daea05e768dc377f701fc768b33
neosiin <rashnikov2.0@gmail.com>
2022-10-12 21:11:07 +0000

    df

--- dist/js/78.eb7955d0.js
+++
@@ -1,6 +0,0 @@
-"use strict";(self["webpackChunkionic_app_base"]=self["webpackChunkionic_app_base"]||[]).push([[78],{78:function(e,t,n){n.r(t),n.d(t,{startStatusTap:function(){return i}});var s=n(65),o=n(74),r=n(587);
-/*!
- * (C) Ionic http://ionicframework.com - MIT License
- */
-const i=()=>{const e=window;e.addEventListener("statusTap",(()=>{(0,s.wj)((()=>{const t=e.innerWidth,n=e.innerHeight,i=document.elementFromPoint(t/2,n/2);if(!i)return;const a=(0,o.a)(i);a&&new Promise((e=>(0,r.c)(a,e))).then((()=>{(0,s.Iu)((async()=>{a.style.setProperty("--overflow","hidden"),await(0,o.s)(a,300),a.style.removeProperty("--overflow")}))}))}))}))}}}]);
-//# sourceMappingURL=78.eb7955d0.js.map
